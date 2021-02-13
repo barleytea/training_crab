b86d7556d96ce2bd13645202e6cae11c725f984e
barleytea <barleytea362@gmail.com>
2021-02-13 17:30:13 +0900

    suppress inevitable warnings.

--- src/controllers/health_check.rs
+++ src/controllers/health_check.rs
@@ -1,5 +1,6 @@
 use actix_web::HttpResponse;
 
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument]
 pub async fn health_check() -> HttpResponse {
     tracing::info!("health check kicked.");
--- src/controllers/training_masters.rs
+++ src/controllers/training_masters.rs
@@ -14,6 +14,7 @@ pub struct TrainingMasterData {
     pub descriptions: String,
 }
 
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument]
 pub async fn create_training_master(
     item: web::Json<TrainingMasterData>,
@@ -32,6 +33,7 @@ pub async fn create_training_master(
     }
 }
 
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument]
 pub async fn find_training_master(
     query: web::Query<HashMap<String, String>>,
